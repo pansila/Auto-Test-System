815186b59989a611a500e1abab68a70d8b24f6b0
lin.zhou <lin.zhou@freethink.cn>
2019-02-20 12:50:48 +0800

    feat: use flask as web server
    Modifications:
    1. use boilerplate from
    https://github.com/cosmic-byte/flask-restplus-boilerplate.git
    
    Tests:
    1. pipenv run server
    
    Change-Id: Id2fa6bd91b45151bb5cdaa2088cfc8a82812659a

---
+++ webrobot/app/test/base.py
@@ -0,0 +1,21 @@
+
+from flask_testing import TestCase
+
+from app.main import db
+from manage import app
+
+
+class BaseTestCase(TestCase):
+    """ Base Tests """
+
+    def create_app(self):
+        app.config.from_object('app.main.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
