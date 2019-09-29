9d94f053d7f2c426472921d51e3a7f6d7c64cb3c
lin.zhou <lin.zhou@freethink.cn>
2019-09-29 17:52:28 +0800

    fix: generate CSR on Linux
    Modifications:
    1. generate CSR on Linux
    
    Tests:
    1. generate CSR on Linux successfully
    
    Change-Id: I51e6fee894b6febbfcba1c9a93c487a01f136d4e

--- robot-test-endpoint/openvpn.py
+++ robot-test-endpoint/openvpn.py
@@ -5,30 +5,42 @@ from pathlib import Path
 def build_req(cert_name, comm_name):
 	if os.name == 'nt':
 		from pexpect import popen_spawn
+
+		line_begin = ''
+		line_end = os.linesep * 2
 		easy_rsa_path = Path('easy-rsa') / 'Windows'
 		shell = 'cmd.exe'
 		child = popen_spawn.PopenSpawn(shell)
 		child.expect('>')
 		child.sendline('chcp 65001')
-		child.expect('\r\n\r\n')
+		child.expect(line_end)
 	elif os.name == 'posix':
+		line_begin = './'
+		line_end = os.linesep
 		easy_rsa_path = Path('easy-rsa') / 'Linux'
 		shell = '/bin/bash'
 		child = pexpect.spawn(shell)
 
 	child.sendline('cd {}'.format(easy_rsa_path))
-	child.expect('\r\n\r\n')
+	child.expect(line_end)
 
-	child.sendline('init-config')
-	child.expect('copied.\r\n\r\n')
+	if os.name == 'nt':
+		child.sendline(line_begin + 'init-config')
+		child.expect('copied.' + line_end)
+	elif os.name == 'posix':
+		child.sendline('find -not -name "*.cnf" -not -type d -exec chmod +x \{\} \\;')
+		child.expect(line_end)
 
-	child.sendline('vars')
-	child.expect('\r\n\r\n')
+	if os.name == 'nt':
+		child.sendline(line_begin + 'vars')
+	elif os.name == 'posix':
+		child.sendline('source vars')
+	child.expect(line_end)
 
-	child.sendline('clean-all')
-	child.expect('\r\n\r\n')
+	child.sendline(line_begin + 'clean-all')
+	child.expect(line_end)
 
-	child.sendline('build-req {}'.format(cert_name))
+	child.sendline(line_begin + 'build-req {}'.format(cert_name))
 	child.expect(']:')  # Country Name
 
 	child.sendline('\n')
